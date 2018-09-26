6bd94390a3e549011cd2aa13e93d4cac728711ce
btoner <ben.toner@wework.com>
2018-09-26 18:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your solution here!
 const drivers = ['Milo', 'Otis', 'Garfield'];
 
+ drivers.push('ben');
 
-
-Function drivers(){
+Function; drivers(){
   drivers.push('ben');
-}
+};
   
   // destructivelyAppendDriver.push('ben');
